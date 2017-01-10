010ec2f957c2d02e2fe591e06ef846ec941f1eab
Carsten Strotmann <carsten@strotmann.de>
2017-01-10 09:53:19 +0100

    Initial version of DNSSEC and RESOLVER logeater

---
+++ logeater-resolvers.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "os"
+        "strings"
+        "sort"
+)
+
+type sortedMap struct {
+        m map[string]int
+        s []string
+}
+
+func (sm *sortedMap) Len() int {
+        return len(sm.m)
+}
+
+func (sm *sortedMap) Less(i, j int) bool {
+        return sm.m[sm.s[i]] > sm.m[sm.s[j]]
+}
+
+func (sm *sortedMap) Swap(i, j int) {
+        sm.s[i], sm.s[j] = sm.s[j], sm.s[i]
+}
+
+func sortedKeys(m map[string]int) []string {
+        sm := new(sortedMap)
+        sm.m = m
+        sm.s = make([]string, len(m))
+        i := 0
+        for key, _ := range m {
+                sm.s[i] = key
+                i++
+        }
+        sort.Sort(sm)
+        return sm.s
+}
+
+func main() {
+        errormap := make(map[string]int)
+        scanner := bufio.NewScanner(os.Stdin)
+        for scanner.Scan() {
+                txt := scanner.Text()
+                i := strings.Index(txt, "resolving")
+                msg := txt[i+11:]
+                errormap[msg]++
+
+        }
+        for _, res := range sortedKeys(errormap) {
+                fmt.Println(errormap[res],":",res)
+        }
+}
